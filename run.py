1f0f409dc5eb9e04cd8f1b3bb5d2a60f8d7a616d
Faris Alasmary <farisalasmary@gmail.com>
2019-05-07 12:10:58 +0300

    simple and clean code

--- run.py
+++ run.py
@@ -6,23 +6,23 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/hotdice', methods = ['GET', 'POST'])
-def hotdice():
+@app.route('/sitebot', methods = ['GET', 'POST'])
+def sitebot():
     request_json = request.get_json(force=True)
     text_input = str(request_json["text"])
 
     dicerange_array = text_input.split()
     
     user_name = str(request_json["user_name"])
-    diceroll = random.randint(0, 100)
-    diceroll_result = user_name + " rolled a %s out of %s" % (str(diceroll), str(100))
-    diceroll_out = {
+    text = f'Hello, {user_name}'
+    
+    response = {
                         "response_type": "ephemeral",
                         "icon_url": "https://funcamp.net/w/dice.png",
                         "username": "vegas",
-                        "text": diceroll_result
+                        "text": text
                    }
-    return jsonify(diceroll_out)
+    return jsonify(response)
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8090, debug=True)
